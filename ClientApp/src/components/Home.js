099a22676ae393457b45ac592a7f34dde8349c3b
starlit-nox <kdkelly.1998@gmail.com>
2023-09-20 09:35:22 -0500

    Reverted Changes

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -4,20 +4,23 @@ import { Container, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 export class Home extends Component {
-    static displayName = Home.name;
-
     constructor(props) {
         super(props);
 
-        this.state = {
-            categories: [
+        const storedCatagories = localStorage.getItem('catagories');
+        const initialCatagories = storedCatagories
+            ? JSON.parse(storedCatagories)
+            : [
                 { name: 'Groceries', route: '/groceries' },
                 { name: 'Clothes', route: '/clothes' },
                 { name: 'Jewelry', route: '/jewelry' },
                 { name: 'Electronics', route: '/electronics' },
                 { name: 'Home Appliances', route: '/homeappliances' },
                 { name: 'Automotive', route: '/automotives' }
-            ],
+            ];
+
+        this.state = {
+            catagories: initialCatagories,
             editModalOpen: false,
             editIndex: -1,
             editedCategoryName: '',
@@ -25,74 +28,127 @@ export class Home extends Component {
         };
     }
 
-    handleDelete = (index) => {
-        const newCategories = [...this.state.categories];
-        newCategories.splice(index, 1);
-        this.setState({ categories: newCategories });
+    handleAddCatagory = () => {
+        const newCategoryName = window.prompt('Enter the new category name:');
+        const newRoute = window.prompt('Enter the route for the new category:');
+
+        if (newCategoryName && newRoute) {
+            const newCatagory = { name: newCategoryName, route: newRoute };
+            // Add new catagory to the state
+            this.setState(prevState => ({
+                catagories: [...prevState.catagories, newCatagory]
+            }));
+        }
     };
 
-    handleEdit = (index) => {
-        const { name, route } = this.state.categories[index];
-        this.setState({ editModalOpen: true, editIndex: index, editedCategoryName: name, editedRoute: route });
+    handleDelete = index => {
+        const { catagories } = this.state;
+        const newCatagories = [...catagories];
+        newCatagories.splice(index, 1);
+        // Update state to reflect deletion
+        this.setState({ catagories: newCatagories });
+    };
+
+    handleEdit = index => {
+        const { name, route } = this.state.catagories[index];
+        this.setState({
+            editModalOpen: true,
+            editIndex: index,
+            editedCategoryName: name,
+            editedRoute: route
+        });
     };
 
     handleSaveEdit = () => {
         const { editIndex, editedCategoryName, editedRoute } = this.state;
-        const newCategories = [...this.state.categories];
-        newCategories[editIndex].name = editedCategoryName;
-        newCategories[editIndex].route = editedRoute; // Update the route
-        this.setState({ categories: newCategories, editModalOpen: false, editIndex: -1, editedCategoryName: '', editedRoute: '' });
+        const newCatagory = { name: editedCategoryName, route: editedRoute };
+        const newCatagories = [...this.state.catagories];
+
+        if (editIndex === -1) {
+            // Add new catagory
+            newCatagories.push(newCatagory);
+        } else {
+            // Edit existing catagory
+            newCatagories[editIndex] = newCatagory;
+        }
+
+        // Update state and close modal
+        this.setState({
+            catagories: newCatagories,
+            editModalOpen: false,
+            editIndex: -1,
+            editedCategoryName: '',
+            editedRoute: ''
+        });
     };
 
     handleModalClose = () => {
-        this.setState({ editModalOpen: false, editIndex: -1, editedCategoryName: '', editedRoute: '' });
+        this.setState({
+            editModalOpen: false,
+            editIndex: -1,
+            editedCategoryName: '',
+            editedRoute: ''
+        });
     };
+
+    componentDidUpdate() {
+        localStorage.setItem('catagories', JSON.stringify(this.state.catagories));
+    }
 
     render() {
         return (
             <div>
                 <h1>Welcome to Commy E-Commerce!</h1>
-
-                {/* Categories */}
+                <Button color="success" onClick={this.handleAddCatagory}>
+                    Add Catagory
+                </Button>
                 <div>
                     <Container>
-                        {this.state.categories.map((category, index) => (
+                        {this.state.catagories.map((catagory, index) => (
                             <Row className="justify-content-between mb-2" key={index}>
                                 <Col sm={{ size: 'auto' }}>
-                                    <Link to={category.route}>
-                                        <Button color="warning">{category.name}</Button>
+                                    <Link to={catagory.route}>
+                                        <Button color="warning">{catagory.name}</Button>
                                     </Link>
                                     <ButtonGroup>
-                                        <Button color="danger" onClick={() => this.handleDelete(index)}>Delete</Button>
-                                        <Button color="info" onClick={() => this.handleEdit(index)}>Edit</Button>
+                                        <Button color="danger" onClick={() => this.handleDelete(index)}>
+                                            Delete
+                                        </Button>
+                                        <Button color="info" onClick={() => this.handleEdit(index)}>
+                                            Edit
+                                        </Button>
                                     </ButtonGroup>
                                 </Col>
                             </Row>
                         ))}
                     </Container>
                 </div>
-
-                {/* Edit Modal */}
                 <Modal isOpen={this.state.editModalOpen} toggle={this.handleModalClose}>
-                    <ModalHeader>Edit Category</ModalHeader>
+                    <ModalHeader>Edit Catagory</ModalHeader>
                     <ModalBody>
                         <Input
                             type="text"
                             value={this.state.editedCategoryName}
-                            onChange={(e) => this.setState({ editedCategoryName: e.target.value })}
+                            onChange={e => this.setState({ editedCategoryName: e.target.value })}
                         />
                         <Input
                             type="text"
                             value={this.state.editedRoute}
-                            onChange={(e) => this.setState({ editedRoute: e.target.value })}
+                            onChange={e => this.setState({ editedRoute: e.target.value })}
                         />
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="primary" onClick={this.handleSaveEdit}>Save</Button>
-                        <Button color="secondary" onClick={this.handleModalClose}>Cancel</Button>
+                        <Button color="primary" onClick={this.handleSaveEdit}>
+                            Save
+                        </Button>
+                        <Button color="secondary" onClick={this.handleModalClose}>
+                            Cancel
+                        </Button>
                     </ModalFooter>
                 </Modal>
             </div>
         );
     }
 }
+
+export default Home;
---
+++ Migrations/20230831171211_Hi.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Commy.Migrations
+{
+    /// <inheritdoc />
+    public partial class Hi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    CatagoryID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Catagories_CatagoryID",
+                        column: x => x.CatagoryID,
+                        principalTable: "Catagories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Catagories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Accessories for cars.", "Automotive" },
+                    { 2, "Items to wear.", "Clothes" },
+                    { 3, "Electrical items.", "Electronics" },
+                    { 4, "Food", "Groceries" },
+                    { 5, "Things for the home.", "Home Appliances" },
+                    { 6, "Accessories to wear.", "Jewelry" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CatagoryID",
+                table: "Products",
+                column: "CatagoryID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DeleteData(
+                table: "Catagories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Catagories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Catagories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Catagories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Catagories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Catagories",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
